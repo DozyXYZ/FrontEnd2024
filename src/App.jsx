1e67fa7cb9779f63a690acce245e95e2f9a9085f
Dozy <dotuan14747@gmail.com>
2024-09-14 09:47:45 +0300

    first upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import css and the component "todolist"
+import './App.css'
+import Todolist from './components/todolist'
+
+// basically run the app
+function App() {
+
+  return (
+    <>
+      <Todolist />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/todolist.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react"; // import useState
+
+function Todolist() {
+    // define state for elements and an array to contain those elements
+    // todo is an object with 2 keys description and duedate
+    const [todo, setTodo] = useState({ description: "", duedate: "" });
+    const [todos, setTodos] = useState([]);
+
+    // function to handle the "Add Todo" button
+    // will send an alert if one or both input field is empty
+    // otherwise, proceed to append the new todo to the todos array and empty the 2 input fields
+    const handleAdd = () => {
+        if (!todo) {
+            alert("Type something first");
+        } else {
+            setTodos([todo, ...todos]);
+            setTodo({ description: "", duedate: "" });
+        }
+    }
+
+    return (
+        <>
+            <h3>My Todos</h3>
+            {/* set state for the todo input */}
+            <input
+                placeholder="Description"
+                value={todo.description}
+                onChange={event => setTodo({ ...todo, description: event.target.value })}
+            />
+            <input
+                placeholder="Due Date"
+                value={todo.duedate}
+                onChange={event => setTodo({ ...todo, duedate: event.target.value })}
+            />
+
+            <button onClick={handleAdd}>Add Todo</button>
+
+            {/* list the todo extract from todos array, use index as unique identifier */}
+            <table>
+                <tbody>
+                    {
+                        todos.map((todo, index) =>
+                            <tr key={index}>
+                                <td>{todo.description}</td>
+                                <td>{todo.duedate}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </>
+    );
+}
+
+export default Todolist;
