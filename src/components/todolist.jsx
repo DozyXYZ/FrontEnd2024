c2c92543a81254cd8f0fa039a3da5f0a154b9ca3
Dozy <dotuan14747@gmail.com>
2024-09-23 08:53:52 +0300

    first commit

--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"; // import useState
 function Todolist() {
     // define state for elements and an array to contain those elements
     // todo is an object with 2 keys description and duedate
-    const [todo, setTodo] = useState({ description: "", duedate: "" });
+    const [todo, setTodo] = useState({ description: "", duedate: "", priority: "" });
     const [todos, setTodos] = useState([]);
 
     // function to handle the "Add Todo" button
@@ -14,8 +14,15 @@ function Todolist() {
             alert("Type something first");
         } else {
             setTodos([todo, ...todos]);
-            setTodo({ description: "", duedate: "" });
+            setTodo({ description: "", duedate: "", priority: "" });
         }
+    }
+
+    const handleDelete = (row) => {
+        console.log("Delete: " + row);
+        // Boolean value to filter the row, the delete row will be exluded from the new array
+        // row index 1 match index 1, row != index is false, row 1 will be excluded from the new array
+        setTodos(todos.filter((_, index) => row != index));
     }
 
     return (
@@ -33,32 +40,42 @@ function Todolist() {
                         onChange={event => setTodo({ ...todo, description: event.target.value })}
                     />
 
+                    <label>Priority: </label>
+                    <input
+                        placeholder="Type description"
+                        value={todo.priority}
+                        onChange={event => setTodo({ ...todo, priority: event.target.value })}
+                    />
+
                     <label>Date: </label>
                     <input
-                        placeholder="Type due date"
+                        type="date"
                         value={todo.duedate}
                         onChange={event => setTodo({ ...todo, duedate: event.target.value })}
                     />
 
                     <button onClick={handleAdd}>Add Todo</button>
 
+                    {/* need to render the todos, handleDelete to the todotable <Todotable todos={todos} handleDelete={handleDelete} /> */}
+
+                    <table>
+                        <tbody>
+                            <tr><th>Due Date</th><th>Description</th></tr>
+                            {
+                                todos.map((todo, index) =>
+                                    <tr key={index}>
+                                        <td>{todo.duedate}</td>
+                                        <td>{todo.description}</td>
+                                        <td><button onClick={() => handleDelete(index)}>Delete</button></td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
 
-            {/* list the todo extract from todos array, use index as unique identifier */}
-            <table>
-                <tbody>
-                    <tr><th>Due Date</th><th>Description</th></tr>
-                    {
-                        todos.map((todo, index) =>
-                            <tr key={index}>
-                                <td>{todo.duedate}</td>
-                                <td>{todo.description}</td>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </table>
         </>
     );
 }
