f7c261550675b63ac44571a8b5553de91d75c021
Dozy <dotuan14747@gmail.com>
2024-10-07 11:38:00 +0300

    MUI date picker set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 // import css and the component "todolist"
-import './App.css'
+// import './App.css' - remove due to use MUI now
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
 import Todolist from './components/todolist'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
 
 // basically run the app
 function App() {
 
   return (
-    <>
+    <Container maxWidth="xl">
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6'>
+            My Todos
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <CssBaseline />
       <Todolist />
-    </>
+    </Container>
   )
 }
 
--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -4,11 +4,22 @@ import { AgGridReact } from 'ag-grid-react';
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
 
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 function Todolist() {
     // define state for elements and an array to contain those elements
     // todo is an object with 2 keys description and duedate
     const [todo, setTodo] = useState({ description: "", duedate: "", priority: "" });
     const [todos, setTodos] = useState([]);
+    const [selectedDate, setSelectedDate] = useState(null);
     const gridRef = useRef();
 
     const [colDefs, setColDefs] = useState([
@@ -24,8 +35,8 @@ function Todolist() {
     // will send an alert if one or both input field is empty
     // otherwise, proceed to append the new todo to the todos array and empty the 2 input fields
     const handleAdd = () => {
-        if (!todo) {
-            alert("Type something first");
+        if (!todo.description || !todo.duedate || !todo.duedate) {
+            alert("Please fill in all fields!");
         } else {
             setTodos([todo, ...todos]);
             setTodo({ description: "", duedate: "", priority: "" });
@@ -35,52 +46,88 @@ function Todolist() {
     const handleDelete = () => {
         if (gridRef.current.getSelectedNodes().length > 0) {
             setTodos(todos.filter((todo, index) =>
-                index != gridRef.current.getSelectedNodes()[0].id))
+                index != gridRef.current.getSelectedNodes()[0].id));
         } else {
-            alert('Select a row first!')
+            alert('Select a row first!');
+        }
+    };
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+        console.log(selectedDate);
+        if (date) {
+            // convert Datejs object to JavaSrcipt object and then to local time string
+            const localString = date.toDate().toLocaleDateString({ day: '2-digit', month: '2.digit', year: 'numeric' });
+            console.log(localString)
+            // set todo.duedate
+            setTodo((todo) => ({ ...todo, duedate: localString }))
+        } else {
+            setTodo((todo) => ({ ...todo, duedate: '' }));
         }
     };
 
     return (
         <>
-            <h3>Simple Todolist</h3>
+
             {/* set state for the todo input */}
-            <div className="box">
-                <div className="box-content">
-                    <div className="box-text">Add Todo:</div>
+            <Stack
+                direction="row"
+                spacing={2}
+                alignItems="center"
+                justifyContent="center"
+                mt={2}
+            >
+                <TextField
+                    label="Description"
+                    value={todo.description}
+                    onChange={event => setTodo({ ...todo, description: event.target.value })}
+                />
 
-                    <input
-                        placeholder="Description"
-                        value={todo.description}
-                        onChange={event => setTodo({ ...todo, description: event.target.value })}
+                <TextField
+                    label="Priority"
+                    value={todo.priority}
+                    onChange={event => setTodo({ ...todo, priority: event.target.value })}
+                />
+
+                {/* MUI Date Picker set up */}
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label="Date"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        format="DD/MM/YYYY"
                     />
+                </LocalizationProvider>
 
-                    <input
-                        placeholder="Priority"
-                        value={todo.priority}
-                        onChange={event => setTodo({ ...todo, priority: event.target.value })}
-                    />
+                {/* <TextField
+                    label="Date"
+                    value={todo.duedate}
+                    onChange={event => setTodo({ ...todo, duedate: event.target.value })}
+                /> */}
 
-                    <input
-                        type="date"
-                        value={todo.duedate}
-                        onChange={event => setTodo({ ...todo, duedate: event.target.value })}
-                    />
+                <Button
+                    variant="contained"
+                    color="success"
+                    startIcon={<AddIcon />}
+                    onClick={handleAdd}>Add Todo
+                </Button>
 
-                    <button onClick={handleAdd}>Add Todo</button>
-                    <button onClick={handleDelete}>Delete</button>
+                <Button
+                    variant="contained"
+                    color="error"
+                    endIcon={<DeleteIcon />}
+                    onClick={handleDelete}>Delete
+                </Button>
 
-                    <div className='ag-theme-material' style={{ height: 500, width: '100%' }}>
-                        <AgGridReact
-                            ref={gridRef}
-                            onGridReady={params => gridRef.current = params.api}
-                            rowData={todos}
-                            columnDefs={colDefs}
-                            rowSelection="single"
-                        />
-                    </div>
-
-                </div>
+            </Stack>
+            <div className='ag-theme-material' style={{ height: 500, width: '100%' }}>
+                <AgGridReact
+                    ref={gridRef}
+                    onGridReady={params => gridRef.current = params.api}
+                    rowData={todos}
+                    columnDefs={colDefs}
+                    rowSelection="single"
+                />
             </div>
 
         </>
